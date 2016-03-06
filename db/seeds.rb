997905289e747d37587259aca3a23a4932ecbbbf
Widua <niyaz-yakupov13@yandex.ru>
2016-03-06 12:36:32 +0300

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description: %q(<p> Ruby is the fastest growing and most exciting dynamic
+                              language out there. If you need to get working programs delivered
+                              fast, you should add Ruby to your toolbox. </p>),
+                image_url: 'ruby.jpg',
+                price: 49.95)
